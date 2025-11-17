471df5703aa099ed164bc7a3e30b5a1161eef9b9
Harnoor Singh <harnoorsingharora2005@gmail.com>
2025-11-17 22:15:48 +0530

    IMPLEMENTED BASIC BACKEND SETUP ALONG WITH WORKOS WEBHOOKS SETUP fix

--- src/app/(protected)/student/page.tsx
+++ src/app/(protected)/student/page.tsx
@@ -1,8 +1,8 @@
-import { requireStudent } from '@/lib/auth';
+
 import StudentDashboard from '@/components/student/StudentDashboard';
 
 export default async function StudentDashboardPage() {
-  await requireStudent();
+  
 
   return <StudentDashboard />;
 }
